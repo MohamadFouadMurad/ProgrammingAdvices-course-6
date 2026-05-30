6efbaa230a06c5ba1e63f1054d3038f24240fb68
Mohamad Fouad Murad <foadmurad022@gmail.com>
2026-05-30 08:57:06 +0300

    Add Bitwise AND and OR operator examples with project file updates

---
+++ Programming advices-course 6-mySolution/02-Miscellaneous/06-Bitwise OR operator.cpp
@@ -0,0 +1,16 @@
+/*
+I learned how numbers are converted to binary, processed, and then converted back to decimal to output the final result using the Bitwise OR operator.
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	int a = 3;
+	int b = 5;
+
+	cout << "Result:" << (a | b);// = 7
+}
